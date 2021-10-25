e8e9babcae338c071f633e1f9097fc006c71de1c
anubhav100rao <anubhav100rao@gmail.com>
2021-10-25 22:36:52 +0530

    Day 5 completed

---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,16 @@
+const Product = require('../models/product')
+
+const getAllProductsStatic = async(req, res) => {
+    const products = await Product.find({})
+    res.status(200).json({ products })
+}
+const getAllProducts = async(req, res) => {
+    console.log(req.query)
+    const products = await Product.find(req.query)
+    res.status(200).json({ products, nhits: products.length })
+}
+
+module.exports = {
+    getAllProductsStatic,
+    getAllProducts,
+}
