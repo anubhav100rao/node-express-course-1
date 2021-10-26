cb62609e0bdf7b9de9e28bc27366418a9da72a07
anubhav100rao <anubhav100rao@gmail.com>
2021-10-26 21:00:14 +0530

    day 6 completed

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,27 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async(req, res) => {
-    const products = await Product.find({})
+    console.log(req.query)
+    const search = 'ab'
+    const products = await Product.find({
+        name: {$regex: search, $options: 'i'}
+    })
     res.status(200).json({ products })
 }
 const getAllProducts = async(req, res) => {
-    console.log(req.query)
-    const products = await Product.find(req.query)
+    const { featured, company, name } = req.query
+    const queryObj = {}
+    if(featured) {
+        queryObj.featured = featured === 'true' ? true: false
+    }
+    if(company) {
+        queryObj.company = company
+    }
+    if(name) {
+        queryObj.name = {$regex: name, $options: 'i'}
+    }
+    console.log(queryObj)
+    const products = await Product.find(queryObj)
     res.status(200).json({ products, nhits: products.length })
 }
 
